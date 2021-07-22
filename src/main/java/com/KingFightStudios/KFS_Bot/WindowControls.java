cf21b094edf0ecbbf68275035e8667e7e521e411
DoctorProctor <robin.baenecke@mail.de>
2021-07-23 00:20:17 +0200

    Initial commit

---
+++ src/main/java/com/KingFightStudios/KFS_Bot/WindowControls.java
@@ -0,0 +1,45 @@
+package com.KingFightStudios.KFS_Bot;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class WindowControls implements WindowListener {
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        if(Main.process != null) {
+            Main.process.destroyForcibly();
+        }
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        if(Main.process != null) {
+            Main.process.destroyForcibly();
+        }
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+}
