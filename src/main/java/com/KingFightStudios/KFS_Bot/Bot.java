0679f1c38caffead8b887eed155b1bb9d45c8a58
DoctorProctor <robin.baenecke@mail.de>
2021-07-27 08:27:52 +0200

    Update

--- src/main/java/com/KingFightStudios/KFS_Bot/Bot.java
+++ src/main/java/com/KingFightStudios/KFS_Bot/Bot.java
@@ -128,7 +128,12 @@ public class Bot extends ListenerAdapter {
     @Override
     public void onGenericUser(GenericUserEvent event) {
         event.getJDA().getGuildById("705831662734540850").loadMembers().onSuccess(members -> {
-            Main.gui.members(members);
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    Main.gui.members(members);
+                }
+            });
         });
     }
 }
